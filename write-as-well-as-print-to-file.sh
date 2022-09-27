d19ac511113879e1871129c84f750b9c4d670caf
Arun Jangir <jangirarun786@gmail.com>
2022-09-27 18:06:56 +0300

    Create write-as-well-as-print-to-file.sh

---
+++ write-as-well-as-print-to-file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This thing only print to file
+echo "volume of cell" > out1.log
+echo "dipole moment"  >> out1.log
+
+# This thing will print as well as write to file 
+echo "volume of cell" | tee out2.log
+echo "dipole moment"  | tee -a out2.log
+
+# By default `tee` override the file `-a` will force it to append 
