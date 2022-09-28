5a84e1d7ac6344300b338dc5a65d1c034d555b08
Arun Jangir <jangirarun786@gmail.com>
2022-09-28 09:38:46 +0300

    Update sed-commands.sh

--- sed-commands.sh
+++ sed-commands.sh
@@ -7,4 +7,11 @@ sed -i "s/ABC/DEF/" input.txt
 sed -i "s/ABC/DEF/g" input.txt
 
 # Change complete line containing txt ABC
-sed -i "/ABC/c\ABC=1" $calcDIR/INCAR
+sed -i "/ABC/c\ABC=1" input.txt
+
+
+# Add I option to GNU sed to case insensitive search
+sed -i "s/word1/word2/I" input.txt
+
+# Only find word ‘love’ and replace it with ‘sick’ if line content a specific string such as FOO
+sed -i -e '/FOO/s/love/sick/' input.txt
